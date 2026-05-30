e44bf47ffc5274bf8b714aa8819715ca9ee60d37
akunablurger <akunablurger@outlook.co.id>
2026-05-30 19:53:10 +0700

    Update swam.sh

--- swam.sh
+++ swam.sh
@@ -1,40 +1,24 @@
 #!/bin/bash
 
+cd /root
+
+# Download jika belum ada
 if [ ! -f "/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI" ]; then
-    # File cpuminer doesn't exist, perform installation
     wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.9/SRBMiner-Multi-2-5-9-Linux.tar.gz
+
     tar -xvf SRBMiner-Multi-2-5-9-Linux.tar.gz
-    cd SRBMiner-Multi-2-5-9
 fi
 
-# Step 2: Create systemd configuration file srb.service
-sudo tee /etc/systemd/system/veco.service <<EOF
-[Unit]
-Description=SRBMiner-MULTI Service
-After=network.target
+# Masuk ke folder miner
+cd /root/SRBMiner-Multi-2-5-9
 
-[Service]
-ExecStart=/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI --disable-gpu --algorithm yespower --pool stratum+tcp://dagnam.xyz:4629 --wallet WZ2dpArp9du6scQ43hgqHL1PJcAaD5CPnT.cloud --password c=SWAMP,mc=SWAMP,zap=SWAMP
-WorkingDirectory=/root
-Restart=always
-RestartSec=3
-User=root
+# Pastikan executable
+chmod +x SRBMiner-MULTI
 
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Step 3: Set permissions on the configuration file
-sudo chmod 644 /etc/systemd/system/veco.service
-
-# Step 4: Reload systemd configuration
-sudo systemctl daemon-reload
-
-# Step 5: Start the srb service
-sudo systemctl start veco.service
-
-# Wait for 10 seconds
-sleep 10
-
-# Check the status of the srb service
-journalctl -fu veco.service
+# Jalankan miner
+./SRBMiner-MULTI \
+    --disable-gpu \
+    --algorithm yespower \
+    --pool stratum+tcp://dagnam.xyz:4629 \
+    --wallet WZ2dpArp9du6scQ43hgqHL1PJcAaD5CPnT.cloud \
+    --password c=SWAMP,mc=SWAMP,zap=SWAMP
